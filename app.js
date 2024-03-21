8e58d41de2f4f56627752babc72192cca562b72f
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-03-21 08:59:33 -0600

    Config dotenv to use env variables

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { config } from "dotenv";
 
-import routes from './src/routes';
-import usersRoutes from './src/routes/users';
+import routes from "./src/routes";
+import usersRoutes from "./src/routes/users";
 import connectToDb from "./src/config/db";
+
+config();
 
 const app = express();
 
@@ -14,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(routes);
-app.use('/users', usersRoutes);
+app.use("/users", usersRoutes);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,8 @@ import { connect } from "mongoose";
 
 const connectToDb = async () => {
   try {
-    const db = await connect(
-      "mongodb+srv://ve_backend:GgPx1ine84dZHVn9@ve-challenge.wgvok8h.mongodb.net/"
-    );
+    const connectionString = process.env.MONGO_URI || "";
+    await connect(connectionString);
     console.log("DB Connected successfully!");
   } catch (error) {
     console.log(error);
