1470603af0903a202ab45f5d8bea1146aa2de0f2
Fabio Guardado <fabio.guardado2012@gmail.com>
2024-03-20 19:54:01 -0600

    Add Routes Config

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import routes from './src/routes';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Express server" });
-});
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
